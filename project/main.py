f4b1782b48ed390379c43a425bdd02afe3f88198
Ludvig En <167233942+LudvigEn@users.noreply.github.com>
2026-09-19 04:35:51 +0200

    Did a lot of changes to the overall structure of main

--- project/main.py
+++ project/main.py
@@ -1,70 +1,204 @@
-import sys
-import time
+import sys # Access system utilities, including exception reporting
+import time # Provide timing and delay functions
 
-import lvgl as lv
-import network
+import lvgl as lv # Import the LVGL graphics library
+import network # Access the boards network interface
 
-from board_lvgl import start_board
-from debug_log import log
+from board_lvgl import start_board # Import the function that initializes the display and touch hardware
+from debug_log import log # Import the applications logging function
 
-from secrets import WIFI_SSID, WIFI_PASSWORD
+from secrets import WIFI_SSID, WIFI_PASSWORD # Load the Wi-Fi network name and password
 
-VERSION = 0.1
+from data import * # Load the data-processing module
+
+VERSION = 0.2 # Version-control
 
 class Application:
     def __init__(self):
         self.display, self.touch, self.handler = start_board()
-        self.tile2_dark = False
-        self.create_ui()
-        self.connect_wifi()
+        self.dark_mode = True # Dark mode toggle, default on
+        self.backgrounds = [] # List of all backgrounds, for ease of access
+        self.borders = [] # List of all borders (like around boxes), for ease of access
+        self.labels = [] # List of all labels, for ease of access
+        self.create_ui() # Function to trigger creation of UI
+        self.connect_wifi() # Function to connect to wifi
         log("student application ready")
 
-    @staticmethod
-    def apply_tile_colors(tile, label, dark):
-        tile.set_style_bg_opa(lv.OPA.COVER, 0)
-        tile.set_style_bg_color(
-            lv.color_hex(0x000000 if dark else 0xFFFFFF), 0
-        )
-        label.set_style_text_color(
-            lv.color_hex(0xFFFFFF if dark else 0x000000), 0
-        )
+    def apply_colors(self):
+        """Function: Actually change label and tile color"""
+        background_color = lv.color_hex(0x000000 if self.dark_mode else 0xFFFFFF) # Background color, if Dark-mode: black, else white
+        foreground_color = lv.color_hex(0xFFFFFF if self.dark_mode else 0x000000) # Foreground (topmost layer) color, if Dark-mode: white, else black
+
+        for widget in self.backgrounds: # For all backgrounds
+            widget.set_style_bg_opa(lv.OPA.COVER, 0) # Make the background fully opaque (ogenomskinlig)
+            widget.set_style_bg_color(background_color, 0) # Change background color to background_color
+
+        for widget in self.borders: # For all borders
+            widget.set_style_bg_opa(lv.OPA.COVER, 0) # Make the background fully opaque (ogenomskinlig)
+            widget.set_style_bg_color(foreground_color, 0) # Change foreground color to foreground_color
+
+        for label in self.labels: # For all labels
+            label.set_style_text_color(foreground_color, 0) # Change foreground color to foreground_color
 
     def on_tile2_clicked(self, _event):
-        self.tile2_dark = not self.tile2_dark
-        self.apply_tile_colors(self.tile2, self.tile2_label, self.tile2_dark)
+        """Function: Toggles dark-mode on tile2..."""
+        self.dark_mode = not self.dark_mode
+        self.apply_colors()
+
+    def toggle_dark_side(self, _event):
+        """Function: Toggles dark-mode on tile2..."""
+        self.dark_mode = self.dark_mode_switch.has_state(lv.STATE.CHECKED)
+        self.apply_colors()
+
+    def create_title_screen(self):
+        """Function: Startup-screen"""
+        self.title_screen = self.tileview.add_tile(0, 0, lv.DIR.RIGHT) # Create a screen on our tileview
+
+        # Create labels for title-screen
+        self.title_screen_label = lv.label(self.title_screen) # Add a label to the Title screen
+        self.title_screen_label_group_name = lv.label(self.title_screen) # Add a label to the Title screen
+        self.title_screen_label_version = lv.label(self.title_screen) # Add a label to the Title screen
+
+        # Heading
+        self.title_screen_label.set_text("L.I.G.M.A.B") # Set label to the name of the project
+        self.title_screen_label.set_style_text_font(lv.font_montserrat_28, 0) # Set font of the label
+        self.title_screen_label.align(lv.ALIGN.CENTER, 0, -100) # Place the label on the tile
+
+        ## Group Name
+        self.title_screen_label_group_name.set_text("Group 15\nLudvig En, Isac Aubert, Gösta Palmqvist,\nMelker Ruben Saar, Bastian Gralén") # Set label to the name of the members
+        self.title_screen_label_group_name.set_style_text_font(lv.font_montserrat_16, 0) # Set font of the label
+        self.title_screen_label_group_name.center() # Place the label on the tile
+
+        ### Version
+        self.title_screen_label_version.set_text(f"Version:{VERSION}") # Set label to the version of the project
+        self.title_screen_label_version.set_style_text_font(lv.font_montserrat_16, 0) # Set font of the label
+        self.title_screen_label_version.align(lv.ALIGN.CENTER, 0, 100) # Place the label on the tile
+
+        #### Add for potential Dark/Light mode
+        self.backgrounds.append(self.title_screen) # Add to list of all backgrounds
+        self.labels.append(self.title_screen_label) # Add to list of all labels
+        self.labels.append(self.title_screen_label_group_name) # Add to list of all labels
+        self.labels.append(self.title_screen_label_version) # Add to list of all labels
+        log("title-screen created")
+
+    def create_departure_screen(self, stop):
+        """Function: Show departures on a specific stop."""
+        self.dep_scr = self.tileview.add_tile(2, 0, lv.DIR.LEFT) # Create a screen on our tileview
+
+        ## Name of bus-stop
+        header = lv.label(self.dep_scr) # Add a label to the Departure screen
+        header.set_text(stop.name) # Name of stop as header
+        header.set_width(450) # Set width of label
+        header.set_pos(75, 10) # Fixed position of label
+        header.set_style_text_font(lv.font_montserrat_28, 0) # Set font of the label
+
+        border_width = 2 # Thickness of box edges
+
+        ## For each departure, we will have a row with 3 columns; Route, Direction and Time
+        for row_index, departure in enumerate(stop.departures):
+            row = lv.obj(self.dep_scr) # Create a box
+            row.set_size(450, 60) # That is the width of the entire screen and 60 high
+            row.set_pos(75, 55 + row_index * 70) # And which Y-postition depends on which row it is.
+
+            #### Add for potential Dark/Light mode
+            self.backgrounds.append(row)
+
+            dep_timestamp = departure.realtime if departure.realtime else departure.scheduled # Realtime if realtime exists, else scheduled
+            dep_time = dep_timestamp[11:19] # As it's a timestamp, we only want the HH:MM:SS part, i.e. excluding the date.
+
+            columns = [ # The 3 columns that will be shown on the departure screen
+                (50, departure.route.designation), # Route
+                (250, departure.route.direction), # Direction ()
+                (150, dep_time) # If delay -> realtime, else scheduled
+            ]
+
+            x = 0
+            for width, value in columns:
+                outline = lv.obj(row) # For each value, we create a box
+                outline.set_size(width, 60) # That is of a variable width (Route does not need to be big)
+                outline.set_pos(x, 0) # And the position is dependent on the size of the box
+
+                #### Add for potential Dark/Light mode
+                self.borders.append(outline)
+
+                inner = lv.obj(outline)
+                inner.set_size(
+                    width - 2 * border_width,
+                    60 - 2 * border_width
+                )
+                inner.set_pos(border_width, border_width)
+                self.backgrounds.append(inner)
+
+                label = lv.label(inner) # We create a label inside the box
+                label.set_width(width - 10) # Width-limitation on our label (so it doesn't spill over)
+                label.set_text(str(value) if value is not None else "") # Put values on labels in boxes
+                label.center() # Center on middle of the box
+                x += width # And then move on the the next box
+
+                #### Add for potential Dark/Light mode
+                self.labels.append(label)
+
+
+        #### Add for potential Dark/Light mode
+        self.labels.append(header) # Add to list of labels
+        self.backgrounds.append(self.dep_scr)
+        log("departure-screen created")
+
+    def create_settings_screen(self):
+        """Function: Create a settings screen"""
+        self.settings_screen = self.tileview.add_tile(1, 0, lv.DIR.LEFT | lv.DIR.RIGHT) # Create a screen on our tileview
+
+        # self.settings_screen_label = lv.label(self.settings_screen) # We label on settings-screen
+        # self.settings_screen_label.set_text("I SHALL BECOME SETTINGS") # That says something (for now)
+        # self.settings_screen_label.set_style_text_font(lv.font_montserrat_28, 0) # With this font
+        # self.settings_screen_label.center() # In the center of the tile
+        # self.settings_screen.add_flag(lv.obj.FLAG.CLICKABLE) # The settings-screen is clickable (for now)
+        # self.settings_screen.add_event_cb(
+        #     self.on_tile2_clicked, lv.EVENT.CLICKED, None # Create an event if user clicks on tile
+        # )
+        dark_mode_header = lv.label(self.settings_screen)
+        dark_mode_header.set_pos(300, 20)
+        dark_mode_header.set_text("Settings")
+        dark_mode_label = lv.label(self.settings_screen)
+        dark_mode_label.set_text("Dark side")
+        dark_mode_label.set_pos(75, 80)
+        self.labels.append(dark_mode_label)
+        self.labels.append(dark_mode_header)
+
+        self.dark_mode_switch = lv.switch(self.settings_screen)
+        self.dark_mode_switch.set_pos(250, 75)
+        if self.dark_mode:
+            self.dark_mode_switch.add_state(lv.STATE.CHECKED)
+
+        self.dark_mode_switch.add_event_cb(
+            self.toggle_dark_side,
+            lv.EVENT.VALUE_CHANGED,
+            None
+        )
+
+
+
+        #### Add for potential Dark/Light mode
+        self.backgrounds.append(self.settings_screen)
+        log("settings-screen created")
 
     def create_ui(self):
         'Function: Creates UI'
-        self.tileview = lv.tileview(lv.screen_active())
-        self.tileview.set_size(600, 450)
-        self.tileview.set_scrollbar_mode(lv.SCROLLBAR_MODE.OFF)
+        api_data = read_data(True) ## IF TRUE, ONLY USE TEST-DATA INSTEAD (i.e. don't use ACTUAL data)
+        self.tileview = lv.tileview(lv.screen_active()) ## Creates frame to add tiles to
+        self.tileview.set_size(600, 450) ## That is the size 600 in x and 450 in y
+        self.tileview.set_scrollbar_mode(lv.SCROLLBAR_MODE.OFF) ## Scrollbar is that thing on your right that you can grab and scroll.
 
-        self.tile1 = self.tileview.add_tile(0, 0, lv.DIR.RIGHT)
-        self.tile2 = self.tileview.add_tile(1, 0, lv.DIR.LEFT)
+        self.create_title_screen() ## Creation of Title-Screen
+        self.create_settings_screen() ## Placeholder creation of Settings-screen
+        self.create_departure_screen(api_data[0]) ## List of departures
 
-        self.tile1_label = lv.label(self.tile1)
-        self.tile1_label2 = lv.label(self.tile1)
-        self.tile1_label3 = lv.label(self.tile1)
-        self.tile1_label.set_text(f"L.I.G.M.A.B")
-        self.tile1_label2.set_text(f"Group 15\nLudvig En, Isac Aubert, Gösta Palmqvist,\nMelker Ruben Saar, Bastian Gralén")
-        self.tile1_label3.set_text(f"Version:{VERSION}")
-        self.tile1_label.set_style_text_font(lv.font_montserrat_28, 0)
-        self.tile1_label2.set_style_text_font(lv.font_montserrat_16, 0)
-        self.tile1_label3.set_style_text_font(lv.font_montserrat_16, 0)
-        self.tile1_label.align(lv.ALIGN.CENTER, 0, -100)
-        self.tile1_label2.center()
-        self.tile1_label3.align(lv.ALIGN.CENTER, 0, 100)
-        self.apply_tile_colors(self.tile1, self.tile1_label, False)
 
-        self.tile2_label = lv.label(self.tile2)
-        self.tile2_label.set_text("Welcome to the workshop")
-        self.tile2_label.set_style_text_font(lv.font_montserrat_28, 0)
-        self.tile2_label.center()
-        self.apply_tile_colors(self.tile2, self.tile2_label, False)
-        self.tile2.add_flag(lv.obj.FLAG.CLICKABLE)
-        self.tile2.add_event_cb(
-            self.on_tile2_clicked, lv.EVENT.CLICKED, None
-        )
+        #### Add for potential Dark/Light mode
+        self.backgrounds.append(self.tileview)
+
+        self.apply_colors()
+        log("UI created")
 
     @staticmethod
     def connect_wifi():
